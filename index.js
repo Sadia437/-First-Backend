258362d0be47d0774dfeca8a0642b12f69ba7cfb
sadia437 <ronjusadia92@gmail.com>
2025-08-26 12:25:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = 7000;
+app.use(cors());  //ai system k  bola hoy middleware , eta use korar fole amader API sob jaygay support korbe.
+app.use(express.json())//frondend er data backend a pathanor jonno call kore dilei hobe
+
+// first-user-database-CURD
+// YQDQ3GsxCNhImbvl
+const uri = "mongodb+srv://first-user-database-CURD:YQDQ3GsxCNhImbvl@cluster0.amfxxji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function bootstrap() {
+    try {
+        await client.connect();
+        const database = client.db('First-database')
+        const userCollection = database.collection('Users');
+
+        // single user
+        app.get('/users/:userId', async (req, res) => {
+            const id = req.params.userId;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
+        // get=read
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const back = await userCollection.find(query).toArray();
+            res.send(back)
+        })
+
+        // post=create
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        // update user
+        app.put('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            const query = { _id: new ObjectId(id) }
+            const option = { upsert: true }
+
+            const updatedDoc = {
+                $set: {
+                    name: user.name,
+                    email: user.email,
+                    age: user.age,
+                    education: user.education
+                }
+            }
+            const result = await userCollection.updateOne(query, updatedDoc, option);
+            res.send(result)
+
+        })
+
+        // delete
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+bootstrap().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Home Route')
+})
+
+app.listen(port, () => {
+    console.log(`Our Backend Run On: ${port}`);
+
+})
+
+
+
+
+
+
+
+
+
+
